f44d3d4309b9af13aec8d2c2072f39c885d521db
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2024-12-18 14:03:56 +0100

    Links updates

--- components/VisionDesign.js
+++ components/VisionDesign.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function VisionSection() {
     const features = [
       {
@@ -46,6 +48,8 @@ export default function VisionSection() {
   
           {/* Call-to-Action */}
           <div className="text-center">
+          <Link href="/contact" className="block">
+
             <h2 className="text-black font-matt text-[24px] lg:text-[28px] lg:leading-[37px] font-[700] mb-4 uppercase">
             lassen Sie uns Ihre Marke gemeinsam neu definieren
             </h2>
@@ -55,6 +59,7 @@ export default function VisionSection() {
             >
               Kontaktieren uns
             </a>
+            </Link>
           </div>
         </div>
       </div>
