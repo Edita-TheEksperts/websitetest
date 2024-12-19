57dd98b4ac1fdc57bbeb458d6934d1a76e02c9ea
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2024-12-19 14:49:45 +0100

    agency

--- pages/agency.js
+++ pages/agency.js
@@ -551,14 +551,13 @@ const Agency = () => {
                           {/* Buttons */}
                           <h2 className="font-matt text-[42px] leading-[45px] font-extrabold uppercase mb-2"> Folgen Sie uns für mehr Einblicke! </h2>
                           <p className="font-matt text-[20px] leading-[28px] mb-10"> Unsere Reise – digital und direkt zu Ihnen.</p>
-`
                           <div className="flex flex-col md:flex-row justify-center items-center gap-4">
                             {/* Instagram Button */}
                             <a
                               href="https://www.instagram.com/theeksperts/"
                               target="_blank"
                               rel="noopener noreferrer"
-                              className="flex items-center justify-center lg:w-[480px] lg:h-[100px] md:w-auto bg-[#0009FF] text-white lg:text-[64px] text-[34px] pr-2 rounded-lg text-lg font-normal transition-transform transform hover:scale-105"
+                              className="flex items-center justify-center lg:w-[480px] lg:h-[100px] md:w-auto bg-[#0009FF] text-white lg:text-[64px] text-[44px] pr-2 rounded-lg text-lg font-normal transition-transform transform hover:scale-105"
                             >
                               <svg xmlns="http://www.w3.org/2000/svg" width="89" height="90" viewBox="0 0 89 90" fill="none" className="mr-2 lg:p-2 p-4">
                                 <g clip-path="url(#clip0_1068_9931)">
@@ -580,7 +579,7 @@ const Agency = () => {
                               href="https://www.linkedin.com/company/the-eksperts/"
                               target="_blank"
                               rel="noopener noreferrer"
-                              className="flex items-center justify-center lg:w-[480px] lg:h-[100px] w-auto bg-[#0009FF] text-white lg:text-[64px] text-[34px] pr-2 rounded-lg shadow-lg text-lg font-normal transition-transform transform hover:scale-105"
+                              className="flex items-center justify-center lg:w-[480px] lg:h-[100px] w-auto bg-[#0009FF] text-white lg:text-[64px] text-[44px] pr-2 rounded-lg shadow-lg text-lg font-normal transition-transform transform hover:scale-105"
                             >
                               <svg xmlns="http://www.w3.org/2000/svg" width="87" height="88" viewBox="0 0 87 88" fill="none" className="mr-2 lg:p-2 p-4">
                                 <g clip-path="url(#clip0_1068_9947)">
