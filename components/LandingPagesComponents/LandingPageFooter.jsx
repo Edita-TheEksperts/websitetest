dbae2a824c97314b3b9a9de4119a51fa6aec297d
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-03-17 10:58:47 +0100

    updates in footer

--- components/LandingPagesComponents/LandingPageFooter.jsx
+++ components/LandingPagesComponents/LandingPageFooter.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 const LandingPageFooter = () => {
     return (
-        <footer className="bg-[#F1F1F1] w-full mx-auto px-2 lg:px-0">
-        <div className="max-w-[1310px] flex-shrink-0 py-2 px-4 mx-auto h-[164px] flex items-center">
+        <footer className="bg-[#F1F1F1] w-full  mx-auto px-2 lg:px-0">
+        <div className="max-w-[1280px] flex-shrink-0 py-2 px-4 mx-auto h-[164px] flex items-center">
           <div className="flex md:flex-row flex-col justify-between md:items-center  items-start w-full">
             <div className="text-[#000] text-right font-[400] font-montserrat text-[15px] font-matt leading-[24px] tracking-[-0.225px]">
               {/* Left Text Section */}
@@ -20,22 +20,12 @@ const LandingPageFooter = () => {
             <div className="flex space-x-[24px] mt-10 md:mt-0">
               {/* Right Navigation Section */}
               <Link
-                href="/impressum"
+                 href="https://www.the-eksperts.com"
+                 target="_blank"
+                 rel="noopener noreferrer"
                 className="text-[#000] text-[15px] font-[400] leading-[24px] font-matt tracking-[-0.225px]"
               >
-                Impressum
-              </Link>
-              <Link
-                href="/disclaimer"
-                className="text-[#000] text-[15px] font-[400] leading-[24px] font-matt tracking-[-0.225px]"
-              >
-                Disclaimer
-              </Link>
-              <Link
-                href="/datenschutz"
-                className="text-[#000] text-[15px] font-[400] leading-[24px] font-matt tracking-[-0.225px]"
-              >
-                Datenschutz
+                www.the-eksperts.com
               </Link>
             </div>
           </div>
