05ec0a7ffb5e0cd759f713a6c41afc3a7af957d5
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-03-18 09:49:58 +0100

    updates

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.the-eksperts.com",
+  generateRobotsTxt: true, // Optional: generates a robots.txt file automatically
+};
--- next-sitemap.config.ts
+++
@@ -1,8 +0,0 @@
-import { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'https://www.the-eksperts.com',
-  generateRobotsTxt: true,
-};
-
-export default config;
