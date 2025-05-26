12781814318e1dcb13c46e8007d2784568d218b3
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-05-26 13:55:07 +0200

    updates

--- pages/blog/nearshoring-mit-the-eksperts.jsx
+++ pages/blog/nearshoring-mit-the-eksperts.jsx
@@ -21,12 +21,21 @@ const KosovaPost = () => {
     <h2 className="lg:text-[80px] text-[40px] font-matt font-[700] text-black tracking-tight lg:leading-[100%] mb-2 lg:mb-0" style={{fontFamily: 'Matt_Trial', color: '#000'}}>
       Nearshoring mit the eksperts:
     </h2>
-    <h3 className="lg:text-[40px] text-[25px] font-matt font-[400] lg:leading-[48px]" style={{fontFamily: 'Matt_Trial', color: '#000'}}>
+    <h3 className="lg:block hidden lg:text-[40px] text-[25px] font-matt font-[400] lg:leading-[48px]" style={{fontFamily: 'Matt_Trial', color: '#000'}}>
+      So profitieren Unternehmen von digitalen Teams aus dem <br></br> DACH-Raum und dem Balkan mit Book Your Eksperts
+    </h3>
+    <h3 className="lg:hidden block lg:text-[40px] text-[25px] font-matt font-[400] lg:leading-[48px]" style={{fontFamily: 'Matt_Trial', color: '#000'}}>
       So profitieren Unternehmen von digitalen Teams aus dem DACH-Raum und dem Balkan mit Book Your Eksperts
     </h3>
-    <p className="text-[#0009FF] text-[20px] mt-4 font-matt font-[900]" style={{color: '#0009FF', fontSize: '28px'}}>
-      Lesedauer: ca. 5 Minuten
-    </p>
+    <div className='mt-[15px] bg-[#0009FF] rounded-[8px] p-[16px] lg:w-[990px]'>
+<h2 className="text-[28px] font-matt text-white mb-4 uppercase">
+  <span className="font-900"><strong>TL;DR</strong></span>{' '}
+  <span className="font-300">(too long; didn't read)</span>
+</h2>
+    <p className="text-white font-[300] text-[16px] lg:text-[20px] font-matt">
+      Nearshoring ist der smarte Mittelweg zwischen Inhouse und Offshoring. Mit the eksperts erhalten Unternehmen Zugriff auf erfahrene Entwickler, Designer, Salesforce-Spezialisten und Projektleiter aus dem DACH-Raum und dem Westbalkan – kosteneffizient, flexibel und auf Augenhöhe.</p>
+    </div>
+  
   </div>
 </section>
         <section>
@@ -42,9 +51,8 @@ const KosovaPost = () => {
         <section className="mb-12 flex flex-col lg:flex-row">
   {/* Left Content */}
   <div className="max-w-[800px] flex-1 mb-8 lg:mb-0">
-    <h2 className="text-[28px] font-matt font-[900] text-black mb-4">TL;DR</h2>
-    <p className="text-black font-[300] text-[16px] lg:text-[20px] mb-[80px] font-matt">
-      Nearshoring ist der smarte Mittelweg zwischen Inhouse und Offshoring. Mit the eksperts erhalten Unternehmen Zugriff auf erfahrene Entwickler, Designer, Salesforce-Spezialisten und Projektleiter aus dem DACH-Raum und dem Westbalkan – kosteneffizient, flexibel und auf Augenhöhe.
+    <p className="text-[#0009FF] text-[20px] lg:text-[28px] uppercase font-matt font-[900] mb-[25px]" style={{color: '#0009FF'}}>
+      Lesedauer: ca. 5 Minuten
     </p>
     <p className="text-black font-[300] text-[16px] lg:text-[20px] font-matt">
       Die Digitalisierung verlangt nach schnellen, skalierbaren Lösungen – aber auch nach Vertrauen, Kommunikation und Qualität. Genau hier kommt Nearshoring ins Spiel: Fachkräfte aus der Nähe, mit ähnlicher Zeitzone, kulturellem Verständnis und starken Skills.
@@ -52,9 +60,9 @@ const KosovaPost = () => {
   </div>
 
   {/* Right Content (LESEWERT Box) */}
-  <div className="rounded-lg w-full lg:w-[280px] mt-8 lg:mt-0 lg:ml-auto">
+  <div className="rounded-lg w-full lg:w-[280px] mt-8 lg:mt-[40px] lg:ml-auto">
     <h3 className="text-[28px] font-matt font-[900] text-black mb-4 uppercase">LESENSWERT:</h3>
-    <ul className="list-disc ml-6 text-black font-[300] text-[18px] font-matt">
+    <ul className="space-y-[12px] list-disc ml-6 text-black font-[300] text-[18px] font-matt">
       <li>
       <a href="/blog/der-unterschatzte-boost" className="text-black hover:underline" target="_blank" rel="noopener noreferrer">
         Der unterschätzte Boost
@@ -110,14 +118,14 @@ const KosovaPost = () => {
 
  
 </section>
-<section className="mt-[65px]">
+<section className="mt-12">
   <p className="text-black font-[300] text-[16px] lg:text-[20px] font-matt lg:leading-[33px]">
     the eksperts ist mehr als nur ein Nearshoring-Anbieter. Wir sind dein Partner für skalierbare, digitale Lösungen<br></br> mit echten Menschen und echtem Einsatz.
   </p>
 </section>
 
           {/* Call to Action */}
-          <section className="mt-[65px] flex flex-col lg:flex-row justify-left lg:items-center gap-4 lg:gap-8">
+          <section className="mt-12 flex flex-col lg:flex-row justify-left lg:items-center gap-4 lg:gap-8">
   <Link href="/kontakt">
   <button className="flex justify-center items-center gap-2 py-3 px-8 text-white text-lg font-medium rounded-[60px] bg-[#0009FF] hover:bg-blue-700" style={{ width: '299px', padding: '11px 0px' }}>
 Sprich mit uns 
@@ -131,7 +139,7 @@ wir zeigen dir, wie mit Book Your Eksperts dein Business beschleunigt wird.  </p
         </section>
 
         {/* Related Posts */}
-  <section className="mt-[180px]">
+  <section className="mt-[120px]">
   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
     {/* Post 1 */}
     <Link href="/blog/der-unterschatzte-boost">
