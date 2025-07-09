08a8ac6b28d12a8630476ab447d9dc544e1af17c
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-07-09 11:38:22 +0200

    updates

--- pages/thank-you.jsx
+++ pages/thank-you.jsx
@@ -12,11 +12,10 @@ const ThankYou = () => {
         />
         <div className="text-center flex flex-col items-center lg:items-start lg:text-left lg:w-[600px]">
           <h1 className="text-[64px] font-[900] text-black font-matt">
-            Thank You!
+            Vielen Dank!
           </h1>
           <p className="mt-[32px] text-black text-[24px] w-[400px] font-light leading-[33px] font-matt">
-            Your message has been successfully submitted. We'll get back to you shortly.
-          </p>
+Ihre Nachricht wurde erfolgreich übermittelt. Wir werden uns in Kürze bei Ihnen melden.          </p>
           <Link href="/">
             <button className="mt-[40px] group relative font-matt flex items-center justify-center px-1 py-1 bg-transparent border-2 border-[#0009FF] text-[#0009FF] rounded-[10px] font-medium text-[20px] transition duration-100 ease-in-out overflow-hidden">
               <div className="w-14 h-14 bg-[#0009FF] rounded-[10px] flex items-center justify-center text-white z-20">
@@ -26,7 +25,7 @@ const ThankYou = () => {
                   <path d="M12 2L22 12L12 22" stroke="white" strokeWidth="2"/>
                 </svg>
               </div>
-              <span className="mx-4 z-20 group-hover:text-white">Back to Home</span>
+              <span className="mx-4 z-20 group-hover:text-white">Besuchen Sie uns!</span>
               <span className="absolute inset-1 bg-[#0009FF] transition-transform duration-500 ease-in-out scale-x-0 origin-left group-hover:scale-x-100 z-10 rounded-[10px]"></span>
             </button>
           </Link>
